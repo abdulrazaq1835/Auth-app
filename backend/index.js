7e09233968e73311ceaa9d9f9c3457f6d6f21022
Abdulrazaq <abdulrazzak1835@gmail.com>
2026-01-16 00:21:19 +0530

    cmtt

--- backend/index.js
+++ backend/index.js
@@ -5,25 +5,38 @@ import cors from "cors";
 import authRoutes from './routes/authRoutes.js'
 import passport from "passport";
 import "./config/passport.js"
+import path from 'path'
+import { get } from "http";
 
 dotenv.config()
 const app = express();
 
 app.use(passport.initialize())
 
-app.use(cors({
+if(process.env.NODE_ENV !== "production"){
+  app.use(cors({
   origin:process.env.UI_URL,
   credentials:true,
   allowedHeaders: ['Content-Type', 'Authorization']
 }))
+}
 
 app.use(express.json());
 
 const PORT = process.env.PORT || 6000;
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+const __dirname  = path.resolve()
+// app.get("/", (req, res) => {
+//   res.send("hello world");
+// });
 app.use('/auth',authRoutes)
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get("/login",(req,res)=>{
+  res.sendFile(path.join(__dirname,"..frontend","dist","index.html"))
+})
+}
 
 connectDB();
 app.listen(PORT, () => {
