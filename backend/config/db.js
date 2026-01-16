af9feb739e4d6b8e3e0742434f49f048408d5b48
Abdulrazaq <abdulrazzak1835@gmail.com>
2026-01-16 10:09:54 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,17 @@
-import sql, { createConnection } from "mysql2/promise";
+import mysql from "mysql2/promise";
 import dotenv from "dotenv";
-import { createPool } from "mysql/promise";
-
 dotenv.config();
 
 let db;
 
 const connectDB = async () => {
   try {
-    db = await createPool({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    });
+   db = mysql.createPool({
+     host: process.env.DB_HOST,
+     user: process.env.DB_USER,
+     password: process.env.DB_PASSWORD,
+     database: process.env.DB_NAME,
+   })
 
     console.log("database connected");
   } catch (error) {
